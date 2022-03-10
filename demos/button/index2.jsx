f7bf4df50b87982af1cbdab2ebc165fdc3f2e32d
jzy <jiaozhiye@163.com>
2022-03-10 12:54:49 +0800

    1234

--- demos/button/index2.jsx
+++ demos/button/index2.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { QmButton, QmForm } from '@jiaozhiye/qm-design-react';
+import { QmButton } from '@jiaozhiye/qm-design-react';
 
 const sleep = async (delay) => {
   return new Promise((resolve) => setTimeout(resolve, delay));
@@ -11,21 +11,9 @@ export default class ButtonDemo extends React.Component {
     console.log('删除成功！');
   };
 
-  state = {
-    items: [
-      {
-        type: 'INPUT',
-        label: 'asdasd',
-        tooltip: 'Label 描述信息',
-        fieldName: 'a',
-      },
-    ],
-  };
-
   render() {
     return (
       <>
-        <QmForm items={this.state.items} />
         <QmButton type="danger" confirm={{}} click={this.clickHandle}>
           删除
         </QmButton>
